7b35668b116664e3bfc8840a92221f15363b3527
rakhimovv <risonyo@gmail.com>
2015-11-18 20:33:01 +0300

    Fixed User model

--- src/users/models.py
+++ src/users/models.py
@@ -6,8 +6,7 @@ from django.core import urlresolvers
 class User(AbstractBaseUser):
     # username = models.CharField(max_length=254, unique=True)
     # USERNAME_FIELD = 'username'
-    avatar = models.ImageField(upload_to='users_avatars')
-
+    avatar = models.ImageField(upload_to='users_avatars', blank=True, null=True)
     def get_absolute_url(self):
         return urlresolvers.reverse('profile', kwargs={'pk': self.pk})
 
